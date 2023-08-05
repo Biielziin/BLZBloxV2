9ea3546cbac30af49ea5b6f8b97719c046b93c7e
Biielziin <84909473+Biielziin@users.noreply.github.com>
2023-08-05 15:31:04 -0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,46 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+  Name = "Temporary DupeWare Script",
+  LoadingTitle = "loading...",
+  LoadingSubtitle = "by @israven",
+})
+
+-- Windows
+
+local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
+
+local Label = Tab:CreateLabel("Status: Disabled")
+
+local Button = Tab:CreateButton({
+  Name = "Start Data Loss",
+  Callback = function()
+    local args = {
+      [1] = "\0" .. string.rep("B", 4200000)
+    }
+  
+    game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SetTutorialDone"):FireServer(unpack(args))
+
+    Rayfield:Notify({
+      Title = "Started Dupe!",
+      Content = "The data loss script has been started!",
+      Duration = 2,
+      Image = 4483362458,
+    })
+
+    Label:Set("Status: Enabled")
+  end,
+})
+
+local Button = Tab:CreateButton({
+  Name = "Rejoin",
+  Callback = function()
+    local ts = game:GetService("TeleportService")
+
+    local p = game:GetService("Players").LocalPlayer
+    
+    ts:Teleport(game.PlaceId, p)
+  end,
+})
+
+local Paragraph = Tab:CreateParagraph({Title = "FAQ", Content = "The remote event that will perform the Data Rollback was developed by @tobias020108back. I just made the interface. The script works to reset the dungeon cooldown and to make trades. Does not work for use in the Dupe Machine!"})
